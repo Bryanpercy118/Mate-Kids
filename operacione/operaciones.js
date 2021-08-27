4bfd89e82db8bc9e940688176c0873d023be1ea1
Bryanpercy118 <bryanpercy77@gmail.com>
2021-08-27 00:35:40 -0500

    primer comit

---
+++ operacione/operaciones.js
@@ -0,0 +1,29 @@
+function sumar() {
+    var num1 = parseInt(document.getElementById('a').value);
+    var num2 = parseInt(document.getElementById('b').value);
+    var suma =num1 + num2;
+    alert('La suma es: '+ suma);
+}
+  
+function restar() {
+    var num1 = parseInt(document.getElementById('a').value);
+    var num2 = parseInt(document.getElementById('b').value);
+    var resta =num1 - num2;
+    alert('La Resta es: '+ resta);
+}
+
+function multiplicar() {
+    var num1 = parseInt(document.getElementById('a').value);
+    var num2 = parseInt(document.getElementById('b').value);
+    var multiplicar = num1 * num2;
+    alert('La suma es: '+ multiplicar);
+}
+
+function dividir() {
+    var num1 = parseInt(document.getElementById('a').value);
+    var num2 = parseInt(document.getElementById('b').value);
+    var dividir =num1 / num2;
+    alert('La suma es: '+ dividir);
+    
+    
+}
